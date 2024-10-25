f1357611a07326dde9ee16181a05e5f4b0e64760
김세진 <wls3983@gmail.com>
2024-10-25 16:41:58 +0900

    파티션 키 컬럼명 수정했습니다

--- ods/ods.car_defects.sql
+++ ods/ods.car_defects.sql
@@ -2,7 +2,7 @@ CREATE TABLE dl_iceberg.ods.car_defects (
   defect_count VARCHAR COMMENT '결함횟수',
   defect_date VARCHAR COMMENT '결함날짜',
   defect_distance VARCHAR COMMENT '결함발생시 주행거리',
-  defect_speed string COMMENT '결함발생시 속도',
+  defect_speed VARCHAR COMMENT '결함발생시 속도',
   crawl_dt VARCHAR COMMENT '크롤링 시간',
   car_name VARCHAR COMMENT '차명',
   car_type VARCHAR COMMENT '차종',
@@ -12,8 +12,8 @@ CREATE TABLE dl_iceberg.ods.car_defects (
   car_engine_capacity VARCHAR COMMENT '엔진배기량',
   car_fuel VARCHAR COMMENT '사용연료',
   defect_content VARCHAR COMMENT '결함내용',
-  crawl_month VARCHAR COMMENT '크롤링 월 파티션 키'
+  part_dt VARCHAR COMMENT '크롤링 월 파티션 키'
 ) WITH (
-  partitioning = ARRAY['crawl_month'],
+  partitioning = ARRAY['part_dt'],
   sorted_by = ARRAY['car_name', 'defect_date']
 );
