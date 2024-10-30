803fe4ee3611c0177f21e152d01135e49ae40993
김세진 <wls3983@gmail.com>
2024-10-30 11:27:07 +0900

    ods rename col

---
+++ ods/ods.press_raw_data.sql
@@ -0,0 +1,11 @@
+create table dl_iceberg.ods.ds_press
+(
+    idx varchar,
+    machine_name varchar,
+    item_no varchar,
+    working_time varchar,
+    press_time_ms varchar,
+    pressure_1 varchar,
+    pressure_2 varchar,
+    pressure_5 varchar
+);
---
+++ ods/ods.welding_raw_data.sql
@@ -0,0 +1,13 @@
+create table dl_iceberg.ods.ds_welding
+(
+    idx varchar,
+    machine_name varchar,
+    item_no varchar,
+    working_time varchar,
+    thickness_1_mm varchar,
+    thickness_2_mm varchar,
+    weld_force_bar varchar,
+    weld_current_ka varchar,
+    weld_voltage_v varchar,
+    weld_time_ms varchar
+);
